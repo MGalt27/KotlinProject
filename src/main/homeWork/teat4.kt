49c6f1b220ceaf21ae03b0317624bb968f2aaa9a
Mgalt27 <mgaltyaev@yandex.ru>
2023-03-26 13:04:32 +0200

    26.03.23 v2

---
+++ src/main/homeWork/teat4.kt
@@ -0,0 +1,23 @@
+shvbsks
+        bsbniosb
+        sfn
+fun newInstance(et): me {
+    val args = Bundle()
+    tm
+    val fragment = me()
+    fragment.arguments = args
+    return fragment
+}t
+
+fun m.f(g): s {
+    g
+    s
+
+    h
+    r
+    h
+    er
+
+    n
+    s
+}
