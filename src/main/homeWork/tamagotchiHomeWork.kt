d6331145bdbacf9dc0f542506a61c2c923ed97b9
Mgalt27 <mgaltyaev@yandex.ru>
2023-03-31 20:47:10 +0200

    31.03.23

--- src/main/homeWork/tamagotchiHomeWork.kt
+++ src/main/homeWork/tamagotchiHomeWork.kt
@@ -21,10 +21,10 @@ class Tamagotchi(_name:String, _animalType:String){
     private var reasonDeath = ""
 
     //private val reset = "\u001B[0m"
-    private val red = "\u001B[31m"
-    private val green = "\u001B[32m"
-    private val yellow = "\u001B[33m"
-    private val blue = "\u001B[34m"
+    private val colorLineRed = "\u001B[31m"
+    private val colorLineGreen = "\u001B[32m"
+    private val colorLineYellow = "\u001B[33m"
+    private val colorLineBlue = "\u001B[34m"
 
     private fun statusAlive(){
         when{
@@ -67,24 +67,24 @@ class Tamagotchi(_name:String, _animalType:String){
                     mood += 10
                     energy -= 10
                     liveTimeHour+=1
-                    println("${yellow}done")
+                    println("${colorLineYellow}done")
                 }
                 (choise == 2) -> {
                     mood += 20
                     energy -= 40
                     liveTimeHour+=1
-                    println("${yellow}done")
+                    println("${colorLineYellow}done")
                 }
                 (choise == 3) -> {
                     mood += 50
                     energy -= 90
                     liveTimeHour+=1
-                    println("${yellow}done")
+                    println("${colorLineYellow}done")
                 }
             }
         }
         else{
-            println("${red}$name is dead")
+            println("${colorLineRed}$name is dead")
         }
     }
 
@@ -95,10 +95,10 @@ class Tamagotchi(_name:String, _animalType:String){
             statusHungry += 5
             mood += 10
             liveTimeHour+=1
-            println("${yellow}done")
+            println("${colorLineYellow}done")
         }
         else{
-            println("$red$name is dead")
+            println("$colorLineRed$name is dead")
         }
     }
 
@@ -127,7 +127,7 @@ class Tamagotchi(_name:String, _animalType:String){
             }
         }
         else{
-            println("$red$name is dead")
+            println("$colorLineRed$name is dead")
         }
     }
 
@@ -139,10 +139,10 @@ class Tamagotchi(_name:String, _animalType:String){
             mood += 20
             energy +=100
             liveTimeHour+=1
-            println("${yellow}done")
+            println("${colorLineYellow}done")
         }
         else{
-            println("$red$name is dead")
+            println("$colorLineRed$name is dead")
         }
     }
 
@@ -150,29 +150,29 @@ class Tamagotchi(_name:String, _animalType:String){
         statusAlive()
         when{
             (statusAlive == "Alive") -> {
-                println("${yellow}***********************\n" +
-                        "${green}name - ${yellow}$name\n" +
-                        "${green}animalType - ${yellow}$animalType\n" +
-                        "${green}liveTimeHour - ${yellow}$liveTimeHour\n" +
-                        "${green}statusHungry - ${yellow}$statusHungry\n" +
-                        "${green}statusToilet - ${yellow}$statusToilet\n" +
-                        "${green}mood - ${yellow}${mood}\n" +
-                        "${green}energy - ${yellow}${energy}\n" +
-                        "${green}statusAlive - ${green}$statusAlive\n" +
-                        "${yellow}***********************\n")
+                println("${colorLineYellow}***********************\n" +
+                        "${colorLineGreen}name - ${colorLineYellow}$name\n" +
+                        "${colorLineGreen}animalType - ${colorLineYellow}$animalType\n" +
+                        "${colorLineGreen}liveTimeHour - ${colorLineYellow}$liveTimeHour\n" +
+                        "${colorLineGreen}statusHungry - ${colorLineYellow}$statusHungry\n" +
+                        "${colorLineGreen}statusToilet - ${colorLineYellow}$statusToilet\n" +
+                        "${colorLineGreen}mood - ${colorLineYellow}${mood}\n" +
+                        "${colorLineGreen}energy - ${colorLineYellow}${energy}\n" +
+                        "${colorLineGreen}statusAlive - ${colorLineGreen}$statusAlive\n" +
+                        "${colorLineYellow}***********************\n")
             }
             else -> {
-                println("${yellow}***********************\n" +
-                        "${green}name - ${yellow}$name\n" +
-                        "${green}animalType - ${yellow}$animalType\n" +
-                        "${green}liveTimeHour - ${yellow}$liveTimeHour\n" +
-                        "${green}statusHungry - ${yellow}$statusHungry\n" +
-                        "${green}statusToilet - ${yellow}$statusToilet\n" +
-                        "${green}mood - ${yellow}${mood}\n" +
-                        "${green}energy - ${yellow}${energy}\n" +
-                        "${green}statusAlive - ${red}$statusAlive\n" +
-                        "${green}reason death - ${red}$reasonDeath\n" +
-                        "${yellow}***********************\n")
+                println("${colorLineYellow}***********************\n" +
+                        "${colorLineGreen}name - ${colorLineYellow}$name\n" +
+                        "${colorLineGreen}animalType - ${colorLineYellow}$animalType\n" +
+                        "${colorLineGreen}liveTimeHour - ${colorLineYellow}$liveTimeHour\n" +
+                        "${colorLineGreen}statusHungry - ${colorLineYellow}$statusHungry\n" +
+                        "${colorLineGreen}statusToilet - ${colorLineYellow}$statusToilet\n" +
+                        "${colorLineGreen}mood - ${colorLineYellow}${mood}\n" +
+                        "${colorLineGreen}energy - ${colorLineYellow}${energy}\n" +
+                        "${colorLineGreen}statusAlive - ${colorLineRed}$statusAlive\n" +
+                        "${colorLineGreen}reason death - ${colorLineRed}$reasonDeath\n" +
+                        "${colorLineYellow}***********************\n")
             }
         }
 
@@ -180,26 +180,26 @@ class Tamagotchi(_name:String, _animalType:String){
 
     fun startGame(){
         var statusGame = true
-        println("${blue}The game is on\n" +
-                "${blue}To exit the game, enter 0")
+        println("${colorLineBlue}The game is on\n" +
+                "${colorLineBlue}To exit the game, enter 0")
         while(statusGame){
-            println("${blue}************************\n" +
-                    "${blue}Game menu:\n" +
-                    "${blue}animal card - 1\n" +
-                    "${blue}eat - 2\n" +
-                    "${blue}toilet - 3\n" +
-                    "${blue}sleep - 4\n" +
-                    "${blue}game - 5\n" +
-                    "${blue}exit - 0\n" +
-                    "${blue}************************\n")
-            print("${yellow}choise - "); var choise = readln().toInt()
+            println("${colorLineBlue}************************\n" +
+                    "${colorLineBlue}Game menu:\n" +
+                    "${colorLineBlue}animal card - 1\n" +
+                    "${colorLineBlue}eat - 2\n" +
+                    "${colorLineBlue}toilet - 3\n" +
+                    "${colorLineBlue}sleep - 4\n" +
+                    "${colorLineBlue}game - 5\n" +
+                    "${colorLineBlue}exit - 0\n" +
+                    "${colorLineBlue}************************\n")
+            print("${colorLineYellow}choise - "); var choise = readln().toInt()
             when{
                 (choise == 1) -> {
                     var statusAnimalCard = true
                     while (statusAnimalCard){
                         animalCard()
-                        println("${blue}back - 0")
-                        print("${yellow}choise - "); var choiseAnimalCard = readln().toInt()
+                        println("${colorLineBlue}back - 0")
+                        print("${colorLineYellow}choise - "); var choiseAnimalCard = readln().toInt()
                         if (choiseAnimalCard == 0){
                             statusAnimalCard = false
                         }
@@ -208,19 +208,19 @@ class Tamagotchi(_name:String, _animalType:String){
                 (choise == 2) -> {
                     var statusEat = true
                     while (statusEat){
-                        println("${blue}1 - firstFood:\n" +
-                                "   ${blue}statusHungry -= 10\n" +
-                                "   ${blue}statusToilet += 5\n" +
+                        println("${colorLineBlue}1 - firstFood:\n" +
+                                "   ${colorLineBlue}statusHungry -= 10\n" +
+                                "   ${colorLineBlue}statusToilet += 5\n" +
                                 "\n" +
-                                "${blue}2 - secondFood:\n" +
-                                "   ${blue}statusHungry -= 20\n" +
-                                "   ${blue}statusToilet += 15\n" +
+                                "${colorLineBlue}2 - secondFood:\n" +
+                                "   ${colorLineBlue}statusHungry -= 20\n" +
+                                "   ${colorLineBlue}statusToilet += 15\n" +
                                 "\n" +
-                                "${blue}3 - thirdFood:\n" +
-                                "   ${blue}statusHungry -= 30\n" +
-                                "   ${blue}statusToilet += 20\n")
-                        println("${blue}back - 0")
-                        print("${yellow}choise - "); var choiseEat = readln().toInt()
+                                "${colorLineBlue}3 - thirdFood:\n" +
+                                "   ${colorLineBlue}statusHungry -= 30\n" +
+                                "   ${colorLineBlue}statusToilet += 20\n")
+                        println("${colorLineBlue}back - 0")
+                        print("${colorLineYellow}choise - "); var choiseEat = readln().toInt()
                         when{
                             (choiseEat == 1) -> { eat(1) }
                             (choiseEat == 2) -> { eat(2) }
@@ -234,19 +234,19 @@ class Tamagotchi(_name:String, _animalType:String){
                 (choise == 5) -> {
                     var statusGame = true
                     while (statusGame){
-                        println("${blue}1 - firstGame:\n" +
-                                "   ${blue}mood += 10\n" +
-                                "   ${blue}energy -= 10\n" +
+                        println("${colorLineBlue}1 - firstGame:\n" +
+                                "   ${colorLineBlue}mood += 10\n" +
+                                "   ${colorLineBlue}energy -= 10\n" +
                                 "\n" +
-                                "${blue}2 - secondGame:\n" +
-                                "   ${blue}mood += 20\n" +
-                                "   ${blue}energy -= 40\n" +
+                                "${colorLineBlue}2 - secondGame:\n" +
+                                "   ${colorLineBlue}mood += 20\n" +
+                                "   ${colorLineBlue}energy -= 40\n" +
                                 "\n" +
-                                "${blue}3 - thirdGame:\n" +
-                                "   ${blue}mood += 50\n" +
-                                "   ${blue}energy -= 90\n")
-                        println("${blue}back - 0")
-                        print("${yellow}choise - "); var choiseGame = readln().toInt()
+                                "${colorLineBlue}3 - thirdGame:\n" +
+                                "   ${colorLineBlue}mood += 50\n" +
+                                "   ${colorLineBlue}energy -= 90\n")
+                        println("${colorLineBlue}back - 0")
+                        print("${colorLineYellow}choise - "); var choiseGame = readln().toInt()
                         when{
                             (choiseGame == 1) -> { game(1) }
                             (choiseGame == 2) -> { game(2) }
@@ -257,7 +257,7 @@ class Tamagotchi(_name:String, _animalType:String){
                 }
                 (choise == 0) -> {
                     statusGame = false
-                    println("${red}game is over")
+                    println("${colorLineRed}game is over")
                 }
             }
         }
